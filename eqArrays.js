2313f6dfabc06d5d95cafe1d97243fd47b825f25
amarkham1 <andrewjmarkham1@gmail.com>
2020-09-15 18:17:50 +0000

    added function to compare array equality

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+const failed = String.fromCodePoint(0x274C);
+const passed = String.fromCodePoint(0x2705);
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${passed}${passed}${passed} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${failed}${failed}${failed} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  let isEqual = true;
+  if (array1.length !== array2.length) {
+    isEqual = false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      isEqual = false;
+    }
+  }
+  
+  return isEqual;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
