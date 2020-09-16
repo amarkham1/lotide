e5d82ef1dc2cba71f7da55ffe992105bba184526
amarkham1 <andrewjmarkham1@gmail.com>
2020-09-16 17:45:05 +0000

    implemented findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const failed = String.fromCodePoint(0x274C);
+const passed = String.fromCodePoint(0x2705);
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${passed}${passed}${passed} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${failed}${failed}${failed} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(objectToSearch, searchValue) {
+  for (const key of Object.keys(objectToSearch)) {
+    if (objectToSearch[key] === searchValue) {
+      return key;
+    }
+  }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue({drama: "Handmaid's Tale"}, "Handmaid's Tale"), "drama");
+assertEqual(findKeyByValue({}, "anything"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, ""), undefined);
