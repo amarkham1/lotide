b6c5ebc30b5136b42da66d09e90848b7b073dfc3
amarkham1 <andrewjmarkham1@gmail.com>
2020-09-16 13:33:39 +0000

    added countOnly function

--- assertEqual.js
+++ assertEqual.js
@@ -8,8 +8,3 @@ const assertEqual = function(actual, expected) {
     console.log(`${failed}${failed}${failed} Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
-assertEqual('hi', 'hi');
-assertEqual('Lighthouse Labs', 'Bootcamp');
-assertEqual(1, 1);
-assertEqual(1, 2);
---
+++ countOnly.js
@@ -0,0 +1,44 @@
+const failed = String.fromCodePoint(0x274C);
+const passed = String.fromCodePoint(0x2705);
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${passed}${passed}${passed} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${failed}${failed}${failed} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item]++;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
