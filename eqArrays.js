0be74a1908ec70f176f43920d46472bf8f023d53
amarkham1 <andrewjmarkham1@gmail.com>
2020-09-18 18:03:26 +0000

    removed bool flag var to just return bool

--- eqArrays.js
+++ eqArrays.js
@@ -10,20 +10,22 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(array1, array2) {
-  let isEqual = true;
   if (array1.length !== array2.length) {
-    isEqual = false;
+    return false;
   }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
-      isEqual = false;
+      return false;
     }
   }
   
-  return isEqual;
+  return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+/* assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); */
