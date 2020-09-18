e317d8cff5b28996073534fd1396b86835c49bc1
amarkham1 <andrewjmarkham1@gmail.com>
2020-09-18 18:26:26 +0000

    implemented recursive eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -9,21 +9,25 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
+const eqArrays = (array1, array2) => {
+  return lengthCheck(array1, array2) && valueCheck(array1, array2) && valueCheck(array2, array1);
+}
+
+const valueCheck = (array1, array2) => {
   for (const key in array1) {
     if (Array.isArray(array1[key])) {
-      if (!eqArrays(array1[key], array2[key]) {
+      if (!eqArrays(array1[key], array2[key])) {
         return false;
       }
-    } else {
-      return true;
+    } else if (!array2[key] || array1[key] !== array2[key]) {
+      return false;
     }
   }
-  
   return true;
+};
+
+const lengthCheck = (object1, object2) => {
+  return object1.length === object2.length;
 };
 
 
@@ -31,7 +35,8 @@ assertEqual(eqArrays([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]), true);
 assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
 assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
 assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
-/* assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); */
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays([], [[]]), false);
